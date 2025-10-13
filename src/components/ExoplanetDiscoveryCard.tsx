045ab45744d6ffd1c98da9cbe1636e762425ea50
xvx-016 <tanmmay2005@gmail.com>
2025-10-13 11:06:30 +0530

    .

--- src/components/ExoplanetDiscoveryCard.tsx
+++ src/components/ExoplanetDiscoveryCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Planet, Star, Zap, TrendingUp, Eye, Target, Brain } from 'lucide-react';
 
 interface ExoplanetData {
